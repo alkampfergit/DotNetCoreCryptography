cd82c0c0c760a0eaac8512fb0285f4aeb60060df
Gian Maria Ricci <alkampfer@nablasoft.com>
2021-04-02 09:09:10 +0200

    Added skeleton project for keyvault

---
+++ src/DotNetCoreCryptography.KeyVault/Controllers/StatusController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCoreCryptography.KeyVault.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public object Get()
+        {
+            _logger.LogDebug("Status Controller Get From user {userName} with auth type {authType}", HttpContext.User.Identity.Name, HttpContext.User.Identity.AuthenticationType);
+            return new
+            {
+                Status = "OK",
+                User = HttpContext.User.Identity.Name,
+            };
+        }
+    }
+}
